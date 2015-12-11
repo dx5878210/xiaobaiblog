881b855b970789a2d1421077e3bc0ffbd840841c
xiaobai <dx5878210@gmail.com>
2015-12-11 17:58:16 +0800

    change bootsrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@
 #!/usr/bin/env python
 
 from flask import Flask
-app=Flask(__name__) 
+app = Flask(__name__)
 app.config.from_object("config")
 
-from app import views,models
-
+from app import views, models
