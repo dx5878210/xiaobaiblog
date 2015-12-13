80b53ab177eedc7f1c493f8a93144240830785c2
xiaobai <dx5878210@gmail.com>
2015-12-13 21:21:09 +0800

    增加

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object("config")
-
+db=SQLAlchemy(app)
 from app import views, models
