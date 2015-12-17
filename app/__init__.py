e23fecce96c06bcae47db815192b99abe46ef7b8
xiaobai <dx5878210@gmail.com>
2015-12-17 18:37:36 +0800

    增加 用户第三方登录以及用户资料

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
+import os
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 from flask import Flask
+from config.default import basedir
+
+
 app = Flask(__name__, instance_relative_config=True)
 # Load the default configuration
 app.config.from_object('config.default')
@@ -10,8 +16,17 @@ app.config.from_pyfile('config.py')
 # app.config.from_envvar('config.')
 
 
+# db config
 db = SQLAlchemy(app)
+
+# user-login config
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 from app import views, models
+
+
 
 
 # start.sh
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from app import db
+from hashlib import md5
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
@@ -10,9 +11,30 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % self.nickname
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + \
+            md5(self.email.encode()).hexdigest() + '?d=mm&s=' + str(size)
 
 
 class Post(db.Model):
